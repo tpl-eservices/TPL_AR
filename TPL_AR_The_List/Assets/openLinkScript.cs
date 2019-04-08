80787e6de12cbfbd59311449484f997145792e95
programthis <kylebachan@gmail.com>
2019-04-08 17:16:55 -0400

    Working on button functionality

---
+++ TPL_AR_The_List/Assets/openLinkScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class openLinkScript : MonoBehaviour, IVirtualButtonEventHandler
+{
+    VirtualButtonBehaviour[] virtualButtonBehaviours;
+    string vbName;
+    // Start is called before the first frame update
+    void Start()
+    {
+         //Register with the virtual buttons TrackableBehaviour
+        virtualButtonBehaviours = GetComponentsInChildren<VirtualButtonBehaviour>();
+ 
+        for (int i = 0; i < virtualButtonBehaviours.Length; i++)
+            virtualButtonBehaviours[i].RegisterEventHandler(this);
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        vbName = vb.VirtualButtonName;
+        if (vbName == "AccountBtn")
+        {
+            Application.OpenURL("https://account.torontopubliclibrary.ca");
+        }
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
