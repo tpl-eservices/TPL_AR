e6ec6f02f7a4b76c410179beacb243bcdca73c89
programthis <kylebachan@gmail.com>
2019-04-09 13:35:26 -0400

    Added timer to start/stop script

--- TPL_AR_The_List/Assets/openLinkScript.cs
+++ TPL_AR_The_List/Assets/openLinkScript.cs
@@ -3,26 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using Vuforia;
 
-public class openLinkScript : MonoBehaviour, IVirtualButtonEventHandler
+public class openLinkScript : MonoBehaviour, ITrackableEventHandler, IVirtualButtonEventHandler
 {
     VirtualButtonBehaviour[] virtualButtonBehaviours;
     string vbName;
-    // Start is called before the first frame update
-    void Start()
-    {
-         //Register with the virtual buttons TrackableBehaviour
-        virtualButtonBehaviours = GetComponentsInChildren<VirtualButtonBehaviour>();
- 
-        for (int i = 0; i < virtualButtonBehaviours.Length; i++)
-            virtualButtonBehaviours[i].RegisterEventHandler(this);
-    }
-
+    
     public void OnButtonPressed(VirtualButtonBehaviour vb)
     {
         vbName = vb.VirtualButtonName;
         if (vbName == "AccountBtn")
         {
-            Application.OpenURL("https://account.torontopubliclibrary.ca");
+            //Application.OpenURL("https://account.torontopubliclibrary.ca");
         }
     }
 
@@ -31,10 +22,150 @@ public class openLinkScript : MonoBehaviour, IVirtualButtonEventHandler
 
     }
 
-
-    // Update is called once per frame
-    void Update()
+    float currCountdownValue;
+    bool startTimer;
+    public IEnumerator StartCountdown(float countdownValue = 3)
     {
-        
+        currCountdownValue = countdownValue;
+        while (currCountdownValue > 0 && startTimer == true)
+        {
+            Debug.Log("Countdown: " + currCountdownValue);
+            yield return new WaitForSeconds(1.0f);
+            currCountdownValue--;
+            Debug.Log(startTimer);
+        }
+        if (startTimer)
+        {
+            Application.OpenURL("https://account.torontopubliclibrary.ca");
+        }
     }
+
+    public void EndCountdown()
+    {
+        currCountdownValue = 3;
+        startTimer = false;
+    }
+
+    #region PROTECTED_MEMBER_VARIABLES
+
+    protected TrackableBehaviour mTrackableBehaviour;
+    protected TrackableBehaviour.Status m_PreviousStatus;
+    protected TrackableBehaviour.Status m_NewStatus;
+
+    #endregion // PROTECTED_MEMBER_VARIABLES
+
+    #region UNITY_MONOBEHAVIOUR_METHODS
+
+    protected virtual void Start()
+    {
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour)
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+
+        //Register with the virtual buttons TrackableBehaviour
+        virtualButtonBehaviours = GetComponentsInChildren<VirtualButtonBehaviour>();
+
+        for (int i = 0; i < virtualButtonBehaviours.Length; i++)
+            virtualButtonBehaviours[i].RegisterEventHandler(this);
+    }
+
+    protected virtual void OnDestroy()
+    {
+        if (mTrackableBehaviour)
+            mTrackableBehaviour.UnregisterTrackableEventHandler(this);
+    }
+
+    #endregion // UNITY_MONOBEHAVIOUR_METHODS
+
+    #region PUBLIC_METHODS
+
+    /// <summary>
+    ///     Implementation of the ITrackableEventHandler function called when the
+    ///     tracking state changes.
+    /// </summary>
+    public void OnTrackableStateChanged(
+        TrackableBehaviour.Status previousStatus,
+        TrackableBehaviour.Status newStatus)
+    {
+        m_PreviousStatus = previousStatus;
+        m_NewStatus = newStatus;
+
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED ||
+            newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+            Debug.Log("Trackable " + mTrackableBehaviour.TrackableName + " found");
+            OnTrackingFound();
+        }
+        else if (previousStatus == TrackableBehaviour.Status.TRACKED &&
+                 newStatus == TrackableBehaviour.Status.NO_POSE)
+        {
+            Debug.Log("Trackable " + mTrackableBehaviour.TrackableName + " lost");
+            OnTrackingLost();
+        }
+        else
+        {
+            // For combo of previousStatus=UNKNOWN + newStatus=UNKNOWN|NOT_FOUND
+            // Vuforia is starting, but tracking has not been lost or found yet
+            // Call OnTrackingLost() to hide the augmentations
+            OnTrackingLost();
+        }
+    }
+
+    #endregion // PUBLIC_METHODS
+
+    #region PROTECTED_METHODS
+
+    protected virtual void OnTrackingFound()
+    {
+        Debug.Log("***************************");
+        Debug.Log("On tracking found starting...");
+        Debug.Log("***************************");
+        startTimer = true;
+        StartCoroutine(StartCountdown());
+        
+        var rendererComponents = GetComponentsInChildren<Renderer>(true);
+        var colliderComponents = GetComponentsInChildren<Collider>(true);
+        var canvasComponents = GetComponentsInChildren<Canvas>(true);
+
+        // Enable rendering:
+        foreach (var component in rendererComponents)
+            component.enabled = true;
+
+        // Enable colliders:
+        foreach (var component in colliderComponents)
+            component.enabled = true;
+
+        // Enable canvas':
+        foreach (var component in canvasComponents)
+            component.enabled = true;
+    }
+
+
+    protected virtual void OnTrackingLost()
+    {
+        Debug.Log("********************");
+        Debug.Log("On tracking lost...");
+        Debug.Log("*********************");
+        EndCountdown();
+
+
+        var rendererComponents = GetComponentsInChildren<Renderer>(true);
+        var colliderComponents = GetComponentsInChildren<Collider>(true);
+        var canvasComponents = GetComponentsInChildren<Canvas>(true);
+
+        // Disable rendering:
+        foreach (var component in rendererComponents)
+            component.enabled = false;
+
+        // Disable colliders:
+        foreach (var component in colliderComponents)
+            component.enabled = false;
+
+        // Disable canvas':
+        foreach (var component in canvasComponents)
+            component.enabled = false;
+    }
+
+    #endregion // PROTECTED_METHODS
 }
