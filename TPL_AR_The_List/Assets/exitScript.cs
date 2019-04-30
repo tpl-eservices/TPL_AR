a4076a7bb6079404889ba95338060ad16025ee25
programthis <kylebachan@gmail.com>
2019-04-30 16:57:09 -0400

    Added exit button to app

---
+++ TPL_AR_The_List/Assets/exitScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class exitScript : MonoBehaviour
+{
+    public void exit()
+    {
+        Debug.Log("Exiting application...");
+        Application.Quit();
+    }
+}
