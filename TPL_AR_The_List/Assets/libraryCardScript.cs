a3609e0caf066938dc8871b5ff241a8da399777d
programthis <kylebachan@gmail.com>
2019-09-10 12:05:37 -0400

    Working on branch script

--- TPL_AR_The_List/Assets/libraryCardScript.cs
+++ TPL_AR_The_List/Assets/libraryCardScript.cs
@@ -25,7 +25,31 @@ public class libraryCardScript : MonoBehaviour
                 case "listBtnIconTrigger":
                     Application.OpenURL("https://www.torontopubliclibrary.ca/teens/the-list.jsp?year=2019#list-container");
                     break;
-                default:
+                case "agincourtBtnIconTrigger":
+                    Application.OpenURL("https://www.torontopubliclibrary.ca/agincourt/");
+                    break;
+                case "scarboroughBtnIconTrigger":
+                    Application.OpenURL("https://www.torontopubliclibrary.ca/scarborough/");
+                    break;
+                case "albionBtnIconTrigger":
+                    Application.OpenURL("https://www.torontopubliclibrary.ca/albion/");
+                    break;
+                case "northyorkBtnIconTrigger":
+                    Application.OpenURL("https://www.torontopubliclibrary.ca/northyorkcentral/");
+                    break;
+                case "downsviewBtnIconTrigger":
+                    Application.OpenURL("https://www.torontopubliclibrary.ca/downsview/");
+                    break;
+                case "richviewBtnIconTrigger":
+                    Application.OpenURL("https://www.torontopubliclibrary.ca/richview/");
+                    break;
+                case "trlBtnIconTrigger":
+                    Application.OpenURL("https://www.torontopubliclibrary.ca/torontoreferencelibrary/");
+                    break;
+                case "fortyorkBtnIconTrigger":
+                    Application.OpenURL("https://www.torontopubliclibrary.ca/fortyork/");
+                    break;
+            default:
                     Debug.Log("No link source detected...");
                     break;
             }
